a846e29d9ceb1ed5f260e7a60992917a26cd2bac
aZeDen <lucasgosselin3101@gmail.com>
2026-03-02 11:02:28 +0100

    Ajout des images dans le gitignore, finition de register et de style.css pour register

--- db_config.php
+++ db_config.php
@@ -1,15 +1,12 @@
 <?php
-// Informations de connexion basées sur votre export MariaDB
-$host    = '127.0.0.1';     // Ou 'localhost'
-$db      = 'php_exam_db';   // Nom exact de votre base
-$user    = 'root';          // Utilisateur par défaut XAMPP/WAMP
-$pass    = '';              // Mot de passe par défaut (vide)
-$charset = 'utf8mb4';       // En accord avec votre COLLATE utf8mb4_general_ci
+$host    = '127.0.0.1';
+$db      = 'php_exam_db';
+$user    = 'root';
+$pass    = '';
+$charset = 'utf8mb4';
 
-// Construction du DSN (Data Source Name)
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-// Options pour rendre PDO plus robuste et sécurisé
 $options = [
     // Active les exceptions pour les erreurs SQL (crucial pour le débug)
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -20,10 +17,8 @@ $options = [
 ];
 
 try {
-    // Création de l'instance de connexion
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-    // En cas d'erreur, on arrête le script et on affiche le message
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
 ?>
